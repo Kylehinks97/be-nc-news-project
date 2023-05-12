042e3a991b8f50966708bc6bdfc50bdc44d2636a
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-05-12 11:14:08 +0200

    added hosting

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,13 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
+const config = {}
+if(ENV === "production") {
+  config.connectionString = process.env.DATABSE_URL
+  config.max = 2
+}
 
-module.exports = new Pool();
+module.exports = new Pool(config);
